fcc6d5ba7c056c5885ceb6f6cb0b65229f975c7e
artur szydlo <artur.szydlo2@gmail.com>
2017-03-19 22:21:38 +0100

    dodanie rozwijanego menu gornego.

--- script.js
+++ script.js
@@ -8,5 +8,34 @@ function showMenu () {
 	$('button.hamburger').click(function(){
 		$('header.page-header').toggleClass('nav-opened');
 	});
-	
+
+	// .expand-menu
+	/*$('.nav-menu li').click(function(){
+		$(this).find('.expand-menu').toggleClass('exp-menu-opened');
+		});
+	*/
+
+	// .expand-menu vol.2
+
+	$('.expand-menu')
+	.hide()
+	.click(function (e) {
+		e.stopPropagation();
+	});
+
+	$('.nav-menu > li').click(function(){
+
+		$('.nav-menu .expand-menu').each(function(i){
+			$('.nav-menu >li > ul').is(':visible').slideUp();
+			console.log('jeden kurwa');
+		});
+
+
+		if(!($(this).find('ul').is(':visible')))
+			{$(this).find('ul').slideDown();console.log('jesem');}
+		else{
+			$(this).find('ul').slideUp();
+
+		}
+	});
 }
