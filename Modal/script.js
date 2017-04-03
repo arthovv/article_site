54a544e03fe32c411f5d7b1f098a8fb861a36f70
Artur-Komputer\Artur <artur.szydlo2@gmail.com>
2017-04-03 22:19:49 +0200

    modal dla widoku na desktopa

--- Modal/script.js
+++ Modal/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+      document.addEventListener("keyup", function (event) {
+        if (event.keyCode === 27) closeModal();
+      }, false);
 
 
     var register = document.querySelector('.createAccount-btn');
@@ -8,42 +10,52 @@ document.addEventListener("DOMContentLoaded", function () {
       howMuch.innerHTML = (maxLength);
     },false);
 
-        var registerClose = document.querySelector('.close');
+    var registerClose = document.querySelector('.close');
     registerClose.addEventListener("click", closeModal, false);
 
 
     var registerButton = document.querySelector('.sign-up');
     registerButton.addEventListener("submit",function(event){
         event.preventDefault();
+        console.log('zamykam');
         window.setTimeout(closeModal, 1000);
     },false);
 
 
     var text = document.querySelector('#fewWords');
+
     const maxLength= text.getAttribute('maxlength');
     const howMuch = document.querySelector('.howMuchChar');
+
     text.addEventListener('keyup', howMuchChar, false);
+
 
     var checkData = document.querySelectorAll('#firstname, #lastname');
     checkData.forEach(checkName);
 
 
-      var mailAdress = document.querySelector('#email');
-      mailAdress.addEventListener('blur', function(){
-        var mailReg = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
-        var inpMail = this.value;
-        if(!mailReg.test(inpMail)){
-          this.style.borderColor = 'red';
-        } else{
-                this.style.borderColor = 'black';
-              } 
-      })
+    var mailAdress = document.querySelector('#email');
+    mailAdress.addEventListener('blur', function(){
+      var mailReg = /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/;
+      var inpMail = this.value;
+      if(!mailReg.test(inpMail)){
+         this.classList.add('wrong-value') ;
+      } else{
+              this.classList.remove('wrong-value') ;
+            } 
+      });
 
+    var inputPassword = document.querySelector('#password');
+    inputPassword.addEventListener('keyup', checkpass);
+
+    var enterPassword = document.querySelector('#enterpass');
+    enterpass.addEventListener('blur',function(){
+      if(!this.value==inputPassword.value) console.log('hasła róznia sie');
+    },false);
 
 
 
 function howMuchChar() {
-    console.log(maxLength);
     var textLength = this.value.length;
     howMuch.innerHTML = (maxLength - textLength);
 }
@@ -62,8 +74,13 @@ function checkName(value,index){
         checkData[index].addEventListener('blur',function(){
           var nameReg = /^[a-zA-Z\s]{3,}$/;
           var inpValue = this.value;
-          if(nameReg.test(inpValue))console.log('wszystko ok');
-            else console.log('popraw');
+          if(!nameReg.test(inpValue))
+            {
+              this.classList.add('wrong-value') ;
+            } else 
+              {
+                this.classList.remove('wrong-value') ;
+              } 
         },false);
 }
 
@@ -80,7 +97,7 @@ var  passwordStrength = function (password) {
       /.{12}/
   ];
 
-  console.log('passmoc');
+
 
   regExpArr.forEach(function (regexp){
     if(regexp.test(password)){
@@ -98,30 +115,11 @@ function checkpass(password){
   var progress = document.querySelector('#passwordComplete');
   var strength = passwordStrength(this.value);
 
-  console.log('checkpass: '+this.value);
  progress.value = strength.value;
  progress.max = strength.max;
- console.log(progress.value);
+ 
 }
 
-var input = document.querySelector('#password');
-input.addEventListener('keyup', checkpass);
 
 });
 
-
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 27) closeModal();
-}, false);
-
-/*function closeModal() {
-    document.body.classList.remove('modal-open');
-    var data = document.querySelectorAll('.sign-up label input');
-    console.log(data.length);
-    for(i=0; i<data.length; i++)
-      {
-        if(data[i].value)
-          data[i].value="";
-      }
-}
-*/
